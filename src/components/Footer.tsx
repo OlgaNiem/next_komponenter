26d18a75031c6d4c03b6aaee8456cc738736994d
OlgaNiem <102143760+OlgaNiem@users.noreply.github.com>
2023-04-20 10:49:41 +0300

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div>
+  
+      {
+        ['email', 'facebook', 'phone'].map(path =>{
+          return(
+            <div key={path}>
+              <Image src={`/${path}.svg`}alt='socialMedia' width='50' height='50'/>        
+            </div>
+          )
+        })
+      }
+
+      <div className='copyright'>
         Copyright 2021 Olha Niemiets
+      </div>
     </div>
   )
 }
